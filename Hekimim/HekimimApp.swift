5a1760912c91c9428e30112ea2ff3a427aff71a2
Nabiyev969 <nabiyev.anar@icloud.com>
2026-02-04 22:05:46 +0400

    Initial Commit

---
+++ Hekimim/HekimimApp.swift
@@ -0,0 +1,17 @@
+//
+//  HekimimApp.swift
+//  Hekimim
+//
+//  Created by Nabiyev Anar on 04.02.26.
+//
+
+import SwiftUI
+
+@main
+struct HekimimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ProfileView()
+        }
+    }
+}
---
+++ Hekimim/ProfileView.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileView.swift
+//  Hekimim
+//
+//  Created by Nabiyev Anar on 04.02.26.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ProfileView()
+}
